bb08c9252581f89facb9774eba12254dca0ec16b
GuiSonsin <sonsinguilherme@gmail.com>
2025-01-20 22:00:57 -0300

    Recuperando qual o tipo da moeda que o usuario selecionou

--- convert-template-main/scripts.js
+++ convert-template-main/scripts.js
@@ -1,7 +1,16 @@
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const form = document.querySelector("form")
 
 // Manipulando o input amount para receber apenas numeros
 amount.addEventListener("input", () => {
     const hasCaractersRegex = /\D+/
     amount.value = amount.value.replace(hasCaractersRegex, "")
 })
+
+// Capturando o evento de submit do formulario
+form.onsubmit = (event) => {
+    // Desativa o recarrecamento automatico da pagina
+    event.preventDefault()
+    console.log(currency.value)
+}
